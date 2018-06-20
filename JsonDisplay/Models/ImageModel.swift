66dbbdb35f391959b3c13a3c621317166975aafc
Mike Haydan <misha.gajdan@gmail.com>
2018-06-20 18:58:55 +0300

    add helpers

--- JsonDisplay/Models/ImageModel.swift
+++ JsonDisplay/Models/ImageModel.swift
@@ -6,4 +6,34 @@
 //  Copyright © 2018 Misha Apps. All rights reserved.
 //
 
-import Foundation
+struct FullImageApiModel: InitializableCodable {
+    let title: String
+    let imageModels: [ImageApiModel]
+    
+    enum CodingKeys: String, CodingKey {
+        case title
+        case imageModels = "rows"
+    }
+}
+
+struct ImageApiModel: InitializableCodable {
+    let title: String?
+    let description: String?
+    let imageUrl: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case title
+        case description
+        case imageUrl = "imageHref"
+    }
+    
+    var imageModel: ImageModel {
+        return ImageModel(title: title ?? "", description: description ?? "", imageUrl: imageUrl ?? "")
+    }
+}
+
+struct ImageModel {
+    let title: String
+    let description: String
+    let imageUrl: String
+}
